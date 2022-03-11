3ac0854d07b10b24a31908e9251a434f1ae4ac58
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2022-03-11 12:52:16 +0000

    functions as props

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import DisplayMessage from "./components/DisplayMessage/DisplayMessage.jsx"
 import UserInput from "./components/UserInput/UserInput.jsx"
+import { useState } from 'react';
 
 const App = () => {
+  //Declare state of the our userName here 
+
+  //Write a function to handle the the onChnage in the user input and pass it down as props. 
+
   return (
     <>
       <h1>Fact finder!</h1>
       <UserInput label="Name" classNames ="user-input--highlight"/>
+      <h2>Hello: {/* Show new userName here */}</h2>
     </>
   );
 }
--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,16 +1,12 @@
-
 import { useState } from "react";
 import "./UserInput.scss"
 const UserInput = ({classNames, label}) => {
-//How can we set the state so the user name changes in the h2 underneath?
+
     return (
-    <>
         <form>
             <label htmlFor="user-input">{label}: </label>
-            <input className={`user-input ${classNames}`}/>
+            <input className={`user-input ${classNames}`} onChange = {() => {/*Add a new onChange function here*/}}/>
         </form>
-        <h2>{/* Show userName here*/}</h2>
-    </>
       );
 }
  
