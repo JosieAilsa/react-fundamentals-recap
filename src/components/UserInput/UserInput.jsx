a4557eacf39e017aa97b81152dd6ffdf9335a993
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2022-03-11 12:28:22 +0000

    props recap

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,13 +1,15 @@
 
 import { useState } from "react";
 import "./UserInput.scss"
-const UserInput = ({ handleUserInput, label}) => {
+const UserInput = () => {
+//1.How can we make this component reusable so that it isn't just an input with the label of name
 
+//2. We can pass down attributes as props too, how could we pass down the prop of user-input--highlight to this component? 
     return (
     <>
         <form>
-            <label htmlFor="userInput">{label}: </label>
-            <input onChange={handleUserInput} id="useInput"/>
+            <label htmlFor="user-input">Name: </label>
+            <input id="user-input--highlight"/>
         </form>
     </>
       );
